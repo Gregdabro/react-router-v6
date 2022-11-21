7fd57007f4ce74b48807ae0728721598a33dc684
Gregdabro <gregmonak@gmail.com>
2022-11-21 13:50:03 +0100

    Вложенные Route

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,16 @@ import withRedux from "./hoc/withRedux";
 import withRouter from "./hoc/withRouter";
 import "react-toastify/dist/ReactToastify.css";
 import { Route, Routes } from "react-router-dom";
+import PostsLayout from "./layouts/PostsLayout";
 
 function App() {
     return (
         <div className='min-h-screen bg-slate-50 dark:bg-slate-900 transition-colors duration-150 flex flex-col'>
             <NavBar />
             <Routes>
-                <Route path='/' element={<MainPage/>} />
-                <Route path='/auth' element={<AuthLayout/>} />
-                {/*<ProtectedRoute path='/posts/:id?' element={<PostsLayout/>} />*/}
+                <Route path='' element={<MainPage/>} />
+                <Route path='auth/*' element={<AuthLayout/>} />
+                <Route path='posts/*' element={<PostsLayout/>} />
                 {/*<Redirect from='*' to='/' />*/}
             </Routes>
 
